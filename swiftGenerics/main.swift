b6af84e464db397ef6db3caacb560b799d804d95
Pedro Luis Cabrera Acosta <pedroluiscabreraacosta@macbook-pro-de-pedro.local>
2018-05-11 00:30:31 -0600

    Initial Commit

---
+++ swiftGenerics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftGenerics
+//
+//  Created by Pedro Luis Cabrera Acosta on 11/5/18.
+//  Copyright © 2018 Pedro Luis Cabrera Acosta. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
